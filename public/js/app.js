55ebcbccfde62272d9e504d77a993d3be1446aa8
madhuribeera <madhuri.beera@credencys.com>
2016-11-24 11:31:49 +0530

    After checking in console

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,25 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('connected to socket');
 });
+
+socket.on('message', function(message) {
+	console.log('New message');
+	console.log(message.text);
+});
+
+//Handles new msg submitting
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+
+});
